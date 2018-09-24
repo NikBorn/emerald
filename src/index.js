469ec97b65f4684018b06c9550d81fda32cef593
Nik Bornemeier <NikolausRB@gmail.com>
2018-09-24 14:15:30 -0600

    install dependencies and setup router

--- src/index.js
+++ src/index.js
@@ -1,8 +1,15 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import * as React from 'react';
+import * as ReactDOM from 'react-dom';
+import { Router } from 'react-router';
+import createHashHistory from 'history/createHashHistory';
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+const hashHistory = createHashHistory({ basename: process.env.PUBLIC_URL });
+
+ReactDOM.render(
+  <Router history={hashHistory}>
+    <App />
+  </Router>,
+  document.getElementById('root')
+);
